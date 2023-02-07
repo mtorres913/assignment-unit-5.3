44d3a437bc34d370284b7e123c8d63e6b37599a9
MIguel Torres <mtorres913@gmail.com>
2023-02-07 17:30:33 -0600

    submitting assignment

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -5,24 +5,30 @@ let collection = [];
 function addToCollection(title, artist, yearPublished){
     let album = [
         {
-            title,
+            title,  
             artist, 
-            yearPublished,
+            yearPublished
         }
     ]
     collection.push(album)
     return album
 }
 
-// function showCollection(array){
-//     console.log(array.length)
-// // - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
-//     for (i=0; i<=array.length;i++){
-//         console.log(array[0],  'by', array[1],  'published in',  array[2])
-//     }
-// }
-// IN WORKK!!!!!
+function showCollection(array){
+    console.log(array.length)
+// - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
+    for (i=0; i<=array.length;i++){
+        console.log(array[i] +  'by' + array[i],  'published in',  array[i])
+    }
+}
 
+function findByArtist(artist){{
+    let artistList = []
+    for (i=0; i < collection.length; i++)
+    artistList.push(album.artist[i])
+}  {   return artistList = []
+}
+}
 // testing addToCollection function
 
 console.log(addToCollection("In My Own Words", "Ne-Yo", "2006")),
@@ -44,6 +50,9 @@ console.log(addToCollection("Recovery", "Eminem", "2010"))
 
 console.log(collection)
 
+// testing showCollection function
 showCollection(collection)
 
+// testing findByArtist function
+findByArtist("Linkin Park")
 
